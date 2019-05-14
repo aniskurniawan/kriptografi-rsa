307db525faf43730a9621d78c7d583aaa0286325
Anis Kurniawan <anis1400018179@webmail.uad.ac.id>
2019-05-14 15:18:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ENKRIPSI DEKRIPSI</title>
-    <link rel="stylesheet" href="assets/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/user.css">
-</head>
-
-<body>
-    <h1 class="text-uppercase text-center">ALGORITMA RSA</h1>
-    <ul class="nav nav-pills categories">
-        <li><a href="kripto/rsa_enkripsi.php">MULAI ENKRIPSI / DEKRIPSI</a></li>
-    </ul>
-</body>
-
-</html>
+<?php
+header("Location: kripto/rsa_enkripsi.php");
+?>
